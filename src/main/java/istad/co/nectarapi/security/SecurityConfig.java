d6477c02910a9dc46e96cbbe7c3b411c3f6efeb7
SisovandaraKong <sisovandara@gmail.com>
2025-12-11 15:47:35 +0700

    security bakong api

--- src/main/java/istad/co/nectarapi/security/SecurityConfig.java
+++ src/main/java/istad/co/nectarapi/security/SecurityConfig.java
@@ -108,7 +108,7 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.DELETE, "/api/v1/orders/**").hasRole("ADMIN")
 
 
-                        .anyRequest().permitAll()
+                        .anyRequest().authenticated()
                 );
 
         httpSecurity.oauth2ResourceServer(oauth2 -> oauth2
