990e5e2785ec52bc30a16e90ac6865f94fef47ae
Sobothty <supercrisz24@gmail.com>
2026-01-06 10:12:56 +0700

    test

--- src/main/java/istad/co/nectarapi/NectarApiApplication.java
+++ src/main/java/istad/co/nectarapi/NectarApiApplication.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditorProvider")
 public class NectarApiApplication {
-
+    //test
     public static void main(String[] args) {
         SpringApplication.run(NectarApiApplication.class, args);
     }
